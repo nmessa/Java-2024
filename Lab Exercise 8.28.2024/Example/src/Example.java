a035dce66b2e5a1f6d0c4821fbef83099544146a
nmessa <nmessa@sau16.org>
2024-08-19 10:23:04 -0400

    Add files via upload

---
+++ Lab Exercise 8.28.2024/Example/src/Example.java
@@ -0,0 +1,22 @@
+/**
+ * @(#)Example.java
+ *
+ * Example application
+ *
+ * @Author: 
+ * @version 1.00 2023/8/30
+ */
+
+public class Example {
+
+    public static void main(String[] args) {
+
+    	//Add code here
+    	
+    }
+}
+
+//Output
+//Hours Worked: 40
+//pay Amount  : 400.0
+//tax Amount  : 40.0
---
+++ Lab Exercise 8.29.2024/Trigonometry/src/Trigonometry.java
@@ -0,0 +1,35 @@
+/**
+ * @(#)Trigonometry.java
+ *
+ * Trigonometry application
+ *
+ * @nmessa
+ * @version 1.00 2024/8/29
+ */
+
+public class Trigonometry {
+
+    public static void main(String[] args) {
+		//Initialize variables
+    	double sinx, cosx, sum, radians;
+    	
+    	//assign the value of 0.5236 to radians
+    	//Add code here
+    	
+    	
+		//Calculate sine and cosine of 0.5236 radians
+    	//Add code here
+    	 
+    	
+    	//Calculate sin^2 + cos^2
+    	//Add code here
+    	
+    	
+    	//Output result
+    	System.out.println("sine: " + sinx + " cosine: " + cosx +
+    		 " sum: " + sum );
+    }
+}
+
+//Output
+//sine: 0.5000010603626028 cosine: 0.866024791582939 sum: 1.0000000000000002
