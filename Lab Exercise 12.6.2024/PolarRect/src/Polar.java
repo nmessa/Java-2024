3e4b7eb0ae83169a4d0ae99833ea5c01a83acf2c
nmessa <nmessa@sau16.org>
2024-12-01 13:30:40 -0500

    Add files via upload

---
+++ Lab Exercise 12.6.2024/PolarRect/src/Polar.java
@@ -0,0 +1,34 @@
+/**
+ * @(#)Polar.java
+ *
+ *
+ * @Author:  
+ * @version 1.00 2024/12/6
+ */
+
+
+public class Polar {
+	//Instance variables
+	private double radius;
+	private double theta;
+	
+    public Polar(double r, double t) {
+    	//Add code here
+		
+    }
+    
+    public Rect polarToRect()
+    {
+    	//Add code here
+		
+    }
+    
+    public String toString()
+    {
+    	String str = Double.toString(radius) + " /_ " + 
+    		Double.toString(theta);
+    	return str;
+    }
+    
+    
+}
